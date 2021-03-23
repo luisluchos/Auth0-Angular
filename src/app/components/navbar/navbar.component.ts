abd7554152b7acd5740fe11982adbe4ebac49efb
Luis Marin <luismgval@gmail.com>
2021-03-23 18:15:27 +0100

    hide, show component if whe are login

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 
 @Component({
@@ -6,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
+
 export class NavbarComponent implements OnInit {
 
-  constructor() {
+  login:boolean= false
+
+  constructor(public auth: AuthService) {
+    auth.isAuthenticated$.subscribe(data => this.login = data) //hide or show when we are login
+    console.log("login",this.login);
+    
+    
 
   
    }
