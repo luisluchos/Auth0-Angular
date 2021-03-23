167c7f797ce8e2c5ddd88881d7d1f402210d6988
Luis Marin <luismgval@gmail.com>
2021-03-23 15:22:31 +0100

    components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PricesComponent } from './components/prices/prices.component';
+import { ProtectedComponent } from './components/protected/protected.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home", component:HomeComponent},
+  {path: "pricing", component: PricesComponent},
+  {path: "protected", component: ProtectedComponent},
+  {path:"**", pathMatch:"full", redirectTo:"home"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
