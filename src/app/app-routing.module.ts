3a9e3d8a4cb04430291ab2132109704932001514
Luis Marin <luismgval@gmail.com>
2021-03-24 12:04:34 +0100

    Guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 import { HomeComponent } from './components/home/home.component';
 import { PricesComponent } from './components/prices/prices.component';
 import { ProtectedComponent } from './components/protected/protected.component';
@@ -7,7 +8,7 @@ import { ProtectedComponent } from './components/protected/protected.component';
 const routes: Routes = [
   {path:"home", component:HomeComponent},
   {path: "pricing", component: PricesComponent},
-  {path: "protected", component: ProtectedComponent},
+  {path: "protected", component: ProtectedComponent, canActivate: [AuthGuard]}, //solo puede entrar si cumple con el guard
   {path:"**", pathMatch:"full", redirectTo:"home"}
 ];
 
